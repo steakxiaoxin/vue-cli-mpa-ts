38cd727a02d52215d220973ed71d98121e119fc7
Leon_zm Huang <leon_zmhuang@tutorabc.com.cn>
2020-07-08 11:45:04 +0800

    feat: 替换进度条,补充gzip

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,17 @@ const fs = require('fs')
 const chalk = require('chalk')
 const { program } = require('commander')
 const TerserPlugin = require('terser-webpack-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const IS_PRD = process.env.APP_ENV === 'prd'
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 program.option('-p, --pages <items>', 'comma for pages', e => e.split(',')).parse(process.argv)
 const inputPages = program.pages
 const log = console.log
+const PORT = 3006
 
 log(chalk.magentaBright('-----------------------'))
 log(chalk.green('NODE_ENV: '), chalk.yellow(process.env.NODE_ENV))
@@ -39,7 +44,6 @@ function getPages() {
 }
 
 const pages = getPages()
-// console.log('pages: ', pages)
 
 const vueConfig = {
   publicPath: IS_PRODUCTION ? '/dist/' : '/',
@@ -48,6 +52,7 @@ const vueConfig = {
   }),
   productionSourceMap: false,
   devServer: {
+    port: PORT,
     before: app => {
       app.get('/', (req, res, next) => {
         for (let pageItem in pages) {
@@ -60,7 +65,7 @@ const vueConfig = {
   chainWebpack: config => {
     config.when(process.env.NODE_ENV === 'development', config => config.devtool('cheap-eval-source-map'))
     config.plugins.delete('progress')
-    config.plugin('simple-progress-webpack-plugin').use(require.resolve('simple-progress-webpack-plugin'))
+    config.plugin('webpackbar').use(require.resolve('webpackbar'))
     // config.when(process.env.NODE_ENV === 'development', config => config.plugin('hard-source-webpack-plugin').use(require.resolve('hard-source-webpack-plugin')))
     config.when(process.env.NODE_ENV === 'development', config => config.optimization.minimizer('terser').use(TerserPlugin))
 
@@ -75,15 +80,20 @@ const vueConfig = {
       })
       return definitions
     })
-
-    // if (process.env.APP_ENV === 'prd') {
-    //   config.plugin('extract-css').tap(() => [
-    //     {
-    //       path: path.join(__dirname, './dist'),
-    //       filename: 'css/[name].[contenthash:8].css',
-    //     },
-    //   ])
-    // }
+  },
+  configureWebpack: config => {
+    let plugins = []
+    if (IS_PRODUCTION && IS_PRD) {
+      plugins.push(
+        new CompressionWebpackPlugin({
+          algorithm: 'gzip',
+          test: /\.(js|css)$/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          minRatio: 0.8,
+        })
+      )
+    }
+    config.plugins = [...config.plugins, ...plugins]
   },
 }
 
